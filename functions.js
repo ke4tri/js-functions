d389feed04d421d8f28f82cc6f3c3bf4b928aa8b
Wayne Collier <ke4tri@gmail.com>
2018-08-25 11:51:44 -0500

    added notes again

--- functions.js
+++ functions.js
@@ -28,3 +28,29 @@ const nomnom = (name, item) => {
 const bearSticks = nuggetizer('bear');
 console.log(nomnom('wayne', bearSticks)); // wayne devoured bear stix
 
+// function numberAdder that takes in a number and
+// return the number plus 3. so input=22, output == . 
+
+// const numberAdder = (num) => {
+//     return num + 3;
+// };
+
+const numberAdder = (num) => {
+    const finalNumber = num + 3;
+    printToDom(`<h2>${finalNumber}</h2>`, 'allTheNumbers');
+};
+
+
+
+// console.log(numberAdder(22));
+// console.log('num: ', numberAdder(22));
+
+const printToDom = (stringToPrint, divId) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML += stringToPrint;
+};
+numberAdder(22);
+numberAdder(25);
+
+printToDom("I am ready for lunch", "feelings")
+
